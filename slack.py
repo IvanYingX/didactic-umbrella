3b454fd4ef4413da741c6652853d6ceebf985f57
Ivan Ying Xuan <58112372+IvanYingX@users.noreply.github.com>
2021-11-12 03:03:42 +0000

    Update slack.py

--- slack.py
+++ slack.py
@@ -6,7 +6,7 @@ from slack_sdk import WebClient
 
 slack_token = sys.argv[1]
 git_token = sys.argv[2]
-repo = sys.argv[3]
+repo = sys.argv[3].split('/')[1]
 
 client = WebClient(slack_token)
 g = Github(git_token)
