eca4c244bb26874c997dbc695ef5924e8e40c104
Ivan Ying Xuan <58112372+IvanYingX@users.noreply.github.com>
2021-11-12 02:59:12 +0000

    Update slack.py

--- slack.py
+++ slack.py
@@ -4,24 +4,22 @@ from github import Github
 from datetime import datetime
 from slack_sdk import WebClient
 
-print(sys.argv)
-# slack_token = os.environ['SLACK_TOKEN']
-# git_token = os.environ['GIT_TOKEN']
-# repo = os.environ['REPO']
+slack_token = sys.argv[1]
+git_token = sys.argv[2]
+repo = sys.argv[3]
 
-# client = WebClient(slack_token)
-# g = Github(git_token)
-# # channel_id = "C02GMMQUQ56"  # Content Operation
-# channel_id = "C02MBCYLF08" # Test Channel
-# message = f'Issues in {repo} opened more than 5 days ago: \n'
+client = WebClient(slack_token)
+g = Github(git_token)
+# channel_id = "C02GMMQUQ56"  # Content Operation
+channel_id = "C02MBCYLF08" # Test Channel
+message = f'Issues in {repo}: \n'
 
-# for issue in g.get_user().get_repo(repo).get_issues():
-#     today = datetime.now()
-#     diff = (today - issue.created_at).days
-#     if diff > 5:
-#         message += ' ' + issue.title + ' : \t' + str(diff) + ' days ago' + '\n'
-#         message += ' ' + issue.html_url + '\n\n'
+for issue in g.get_user().get_repo(repo).get_issues():
+    today = datetime.now()
+    diff = (today - issue.created_at).days
+    message += ' ' + issue.title + ' : \t' + str(diff) + ' days ago' + '\n'
+    message += ' ' + issue.html_url + '\n\n'
 
-# result = client.chat_postMessage(
-#                     channel=channel_id,
-#                     text=message)
+result = client.chat_postMessage(
+                    channel=channel_id,
+                    text=message)
